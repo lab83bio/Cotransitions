4696c75c95aea36a8c12badea8176552e44291a6
marcomala46 <marcomala46@gmail.com>
2022-09-09 15:56:43 +0200

    install

--- install.sh
+++ install.sh
@@ -1,8 +1,4 @@
 #!/usr/bin/env bash
 conda create  --name cotr --file cotr.txt
 conda activate cotr
-<<<<<<< HEAD
 Rscript -e 'install.packages("corpora",repos="http://cran.us.r-project.org")'
-=======
-Rscript -e 'install.packages("corpora")'
->>>>>>> 06c1b8e4f79196087a39a889e3d9bfe052b3b61e
