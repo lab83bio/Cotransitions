babf7e8b968245994db1ac7f453014467fb6780f
Percud <riccardo.percudani@gmail.com>
2022-09-15 16:19:12 +0200

    second commit

---
+++ Utilities/procedure_mcl_clusters.pl
@@ -0,0 +1,67 @@
+#!/usr/bin/perl -w
+#
+# Clusterize annotated results ".transitions.annotated" (with score and probabilities) with mcl
+#
+use strict;
+#use Getopt::Long;
+
+my $usage="$0 cvs_file transition_file\n";
+
+my $CUT_OFF=1e-3; #min probability
+my $Inf=2.5; #inflation parameter for mcl (eg. 1.4, 2, 4,6) - 2=default
+my $skip_negatives = 1;
+
+my($csv,$transitions)=(shift,shift);
+my $org=$1 if ($transitions=~/(\S+).transitions.*annotated/) or die ("not a .transitions.annotated file");
+open T, "<$transitions" or die $!;
+
+open MCL, ">$org.mcl_input" or die $!;
+
+# write mcl input (cluster1 cluster2 score)
+my %Score;
+foreach (<T>){
+	my @A=split /\t/;
+	next if($A[8]!~/\d/ or $A[8]<0 and $skip_negatives);
+	if($A[10]=~/\d/ and $A[10]<$CUT_OFF){
+		$A[10]=1e-300 if ($A[10]<1e-300);
+		printf MCL "$A[0]\t$A[1]\t%s\n", -(log($A[10])/log(10)); #negative base10 log of the expectation value (NLE)
+		$Score{$A[0]}{$A[1]}=$Score{$A[1]}{$A[0]}=$A[8];
+	}
+}
+close MCL; close T;
+
+#MCL clustering 
+system ("mcl $org.mcl_input -I $Inf --abc -o $org.clusters");
+printf STDERR "mcl input in $org.mcl_input\n";
+printf STDERR "written $org.clusters\n";
+
+
+#annotate with descriptions
+open CSV, "<$csv" or die $!;
+my (%D,%G);
+foreach (<CSV>){
+  my @A=split /\t/;
+    my $genes=grep /\S/, @A[2..$#A]; # number of genes; 
+    $G{$A[0]}=$genes;
+    $D{$A[0]}=$A[1]; #description
+}
+close CSV;
+
+open CLU, "<$org.clusters" or die $!;
+open CLU_ANN, ">$org.clusters.annotated" or die $!;
+foreach (<CLU>){
+  chomp;
+  my @B=split;
+  next if ($#B<1); #skip mono clusters
+  my $t_score=0;
+  for (my $i=0;defined $B[$i+1];$i++) { 
+     for (my $t=$i+1;defined $B[$t];$t++) { 
+		if (defined $Score{$B[$i]}{$B[$t]}){ $t_score+=$Score{$B[$i]}{$B[$t]} } else {$t_score+=0.1} 
+	}
+  }
+  printf CLU_ANN "%.5f %s \(%s\): ", $t_score/( (scalar @B * (scalar @B -1))/2 ),$G{$B[0]}, scalar @B if (scalar @B >=2); 
+  foreach (@B){printf CLU_ANN "%s:%s ### ", $_,$D{$_}  } 
+  printf CLU_ANN "\n";
+}
+close CLU; close CLU_ANN;
+printf STDERR "written $org.clusters.annotated\n";
---
+++ cotr_Fisher.r
@@ -0,0 +1,65 @@
+#!/usr/bin/env Rscript 
+#
+# Fisher probability from a gene transition table 
+#
+library(data.table)# Fast fread
+library(corpora)   # Fast fisher.pval
+library(argparse) 
+
+ap <- ArgumentParser()
+ap$add_argument("-p", "--p.cutoff", type="double", default=1e-3, help="p cutoff")
+ap$add_argument("-pa", "--padj.cutoff", type="double", default=1e-3, help="p.adj cutoff")
+ap$add_argument("csv", nargs=1, help="*.transitions tsv file")
+ 
+args <- ap$parse_args()
+
+csv<-args$csv;
+if (csv == '-') csv='file:///dev/stdin' 
+
+p.cutoff=args$p.cutoff
+padj.cutoff=args$padj.cutoff
+
+##===================================
+##    Read *.transitions file       =
+##===================================
+
+column_classes <- c(rep("factor",2),rep("integer",6))
+D <- fread(csv, header=T, colClasses = column_classes, showProgress=T)
+
+##===================================
+##     Calculate scores             =
+##===================================
+
+D$k_score <- D$k/(D$t1+D$t2-abs(D$k)) # Morandin formula
+
+##===================================
+##   Calculate p.values and p.adj   =
+##===================================
+write(paste0("calculating probabilities (output table to STDOUT)"), stderr())
+norg <- D$orgs
+k <- abs(D$k)
+M<-as.matrix(cbind(k, D$t2, D$t1-k, norg-D$t2)) #k1 n1 k2 n2 for fisher.pval
+p=c() # vector of p.values 
+for(i in 1:nrow(M)) 
+  p[i] <- tryCatch(fisher.pval(M[i,1],M[i,2],M[i,3],M[i,4], alternative="greater"), 
+                   error=function(error_message){return(1)})
+
+p.adj <- p.adjust(p, method="holm")
+
+##=========================================
+##  Write results if p and pa < p.cutoff  =
+##=========================================
+
+D <- cbind(D,p,p.adj)
+D <- D[p.adj <= padj.cutoff,]
+D <- D[p <= p.cutoff,]
+D <- D[order(D$p),]
+
+write.table(format(D,digits=5), file = "" , quote=F, sep="\t", row.names=F)
+
+#all done:
+write(paste0(nrow(D) ," gene pairs with p <= ",p.cutoff, " and p.adj <= ",padj.cutoff), stderr())
+
+
+
+
