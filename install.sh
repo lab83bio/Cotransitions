bfe4bbdad32948a4783b0e5fd52eb70a5ac278b0
Percud <riccardo.percudani@gmail.com>
2022-09-15 16:15:45 +0200

    second commit

--- install.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-conda create  --name cotr --file cotr.txt
-conda activate cotr
-Rscript -e 'install.packages("corpora",repos="http://cran.us.r-project.org")'
-pip3 install git+https://github.com/mojaie/pygosemsim.git
