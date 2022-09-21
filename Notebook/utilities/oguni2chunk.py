d58b91d97d100ee246963ef9e30e691bf42e0724
lab83bio <lab83bio@gmail.com>
2022-09-21 14:21:54 +0200

    update

--- Notebook/utilities/oguni2chunk.py
+++ Notebook/utilities/oguni2chunk.py
@@ -18,18 +18,18 @@ print(sys.argv[1:])
 abspath = os.path.dirname(os.path.abspath(oguninfile))
 
 os.chdir(abspath)
-os.mkdir('uni_go')
+# os.mkdir('uni_go')
 
-chunk_size=500000
-batch_no=1
-dfog = pd.read_csv(os.path.basename(oguninfile), 
-                   chunksize=chunk_size, 
-                   comment='!', 
-                   compression='gzip', 
-                   sep ='\t', header=None)
-for chunk in tqdm.tqdm(dfog):
-    chunk.to_csv(f'uni_go/chunk{str(batch_no)}.csv', index=False, sep ='\t')
-    batch_no+=1
+# chunk_size=500000
+# batch_no=1
+# dfog = pd.read_csv(os.path.basename(oguninfile), 
+#                    chunksize=chunk_size, 
+#                    comment='!', 
+#                    compression='gzip', 
+#                    sep ='\t', header=None)
+# for chunk in tqdm.tqdm(dfog):
+#     chunk.to_csv(f'uni_go/chunk{str(batch_no)}.csv', index=False, sep ='\t')
+#     batch_no+=1
 
 og_uni = pd.read_table(os.path.basename(oguni))
 
@@ -37,12 +37,13 @@ def merge_chunk_unigo(x):
     df = pd.read_table(x, low_memory=False)
     ch = og_uni.merge(df, left_on='id', right_on='1'
                      ).groupby(['og', '4', '8'])['6'].apply(
-        lambda x: list(set(x))[0]).reset_index()
+        lambda x:','.join(list(set(x)))).reset_index()
     return ch
 
 total = (pd.concat(list(tqdm.tqdm(Pool(int(proc)).imap(merge_chunk_unigo, 
                                  glob.glob('uni_go/chunk*.csv')) , total=len(glob.glob('uni_go/chunk*.csv'))))
                   ).drop_duplicates())
+
 total.to_csv(ogunigo, 
                  sep ='\t', 
                  index=False, 
