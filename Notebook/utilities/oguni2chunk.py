c90dc43602a828611e073caaf39d58e5e9065e0b
lab83bio <105450422+lab83bio@users.noreply.github.com>
2022-09-30 17:40:36 +0200

    Update oguni2chunk.py

--- Notebook/utilities/oguni2chunk.py
+++ Notebook/utilities/oguni2chunk.py
@@ -35,17 +35,31 @@ og_uni = pd.read_table(os.path.basename(oguni))
 
 def merge_chunk_unigo(x):
     df = pd.read_table(x, low_memory=False)
-    ch = og_uni.merge(df, left_on='id', right_on='1'
-                     ).groupby(['og', '4', '8'])['6'].apply(
-        lambda x:','.join(list(set(x)))).reset_index()
+    ch = og_uni.merge(df, left_on='id', right_on='1')
     return ch
 
 total = (pd.concat(list(tqdm.tqdm(Pool(int(proc)).imap(merge_chunk_unigo, 
-                                 glob.glob('uni_go/chunk*.csv')) , total=len(glob.glob('uni_go/chunk*.csv'))))
+                                 glob.glob('testing/external/uni_go/chunk*.csv')) , 
+                                  total=len(glob.glob('testing/external/uni_go/chunk*.csv'))))
                   ).drop_duplicates())
 
-total.to_csv(os.path.basename(ogunigo), 
-                 sep ='\t', 
-                 index=False, 
-                 header=['og', 'go', 'type', 'evidence']
-            )
+
+ch_gr = pd.merge(total.groupby(['og', '8','4'])['6'].apply(list).reset_index(),
+                  total.groupby(['og', '8','4'])['id'].apply(list).reset_index())
+ch_gr['len'] = ch_gr['6'].apply(len)
+ch_gr['len_set'] = ch_gr['id'].apply(lambda x: len(set(x)))
+
+def listone(og):
+    d = ch_gr[ch_gr['og']==og]
+    maxs = dict(d.groupby(['og', '8']).max().reset_index()[['8', 'len_set']].values)
+    d['ratio'] = d.apply(lambda x: x['len_set']/maxs.get(x['8']), axis=1)
+    return d
+    
+totalistone = list(tqdm.tqdm(Pool(100).imap(listone, set(ch_gr['og'])), total=len(set(ch_gr['og']))))
+
+
+pd.concat(totalistone).to_csv(f"{os.path.basename(ogunigo)}.unigo", 
+                              index=False, 
+                              sep='\t', 
+                              header=['og', 'type', 'go', 'evidence', 'uni', 'len', 'len_set','ratio']
+                             )
